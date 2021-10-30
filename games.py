72f7c0d5f7e530faf3e03b5c94d620e15d6358ad
ZAXID <ssvlsdislav2007@gmail.com>
2021-10-30 18:20:59 +0200

    add_1_part

--- games.py
+++ games.py
@@ -1,21 +1,14 @@
-#Модуль gemes
-# Демонстрирует сосдание модуля
-
 def ask_yes_no(question):
-    """топрос да или нет"""
-    reseponse = None
-    while reseponse not in ("y", "n"):
-        reseponse = input(question + ' (y/n)? ').lower
-
-#
-
+    response = None
+    while response not in ("y", "n"):
+        response = input(question + ' (y/n)? ').lower()
+        return response
 def ask_number(question, low, high):
-    """Просит вести число из диапозона"""
     response = None
     while response not in range(low, high + 1):
         response = int(input(question))
     return response
 
 if __name__ == "__main__":
-    print("Вы запустили модуль games")
-    input("\n\nНамите Enter, чтобы выйти.")
+    print("вы запустили модуль games, а не импортировали его")
+    input("\n\nажмите Enter, чтобы выйти")
